0eae17b578385c82238b80baae5839d8c412e8a4
PopescuTudor <tudorcristian.popescu@yahoo.com>
2022-12-11 07:01:17 +0200

    merged_1

--- views/javaS.js
+++ views/javaS.js
@@ -3,9 +3,9 @@ const contentDisposition = require("content-disposition");
 function changeImage() {
     console.log(document.getElementById("imgClickAndChange").src);
     if (document.getElementById("imgClickAndChange").src.includes( "ro.png")){
-        document.getElementById("imgClickAndChange").src = "images/gb.png";
+        document.getElementById("imgClickAndChange").src = "../images/gb.png";
     } else {
-        document.getElementById("imgClickAndChange").src = "images/ro.png";
+        document.getElementById("imgClickAndChange").src = "../images/ro.png";
     }
 }
 function changeText(clicked_id) {
@@ -114,8 +114,41 @@ function changeText(clicked_id) {
             console.log("default");
 
     }
-    
-    
+}
 
+var captcha;
+function generate() {
+  // Clear old input
+  document.getElementById("submit").value = "";
 
+  // Access the element to store
+  // the generated captcha
+  captcha = document.getElementById("image");
+  var uniquechar = "";
+
+  const randomchar =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  // Generate captcha for length of
+  // 5 with random character
+  for (let i = 1; i < 5; i++) {
+    uniquechar += randomchar.charAt(Math.random() * randomchar.length);
+  }
+
+  // Store generated input
+  captcha.innerHTML = uniquechar;
+}
+
+function printmsg() {
+  const usr_input = document.getElementById("submit").value;
+
+  // Check whether the input is equal
+  // to generated captcha or not
+  if (usr_input == captcha.innerHTML) {
+    var s = (document.getElementById("key").innerHTML = "Cod corect");
+    location.replace("../pagini/subjectOptions.html");
+  } else {
+    var s = (document.getElementById("key").innerHTML = "Cod greșit");
+    generate();
+  }
 }
